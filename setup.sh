740bbda4238d6b6a4ab43e5e851ec363b1dbb4f6
sta <sinisa.tatomir@cyberhouse.at>
2015-02-19 10:14:03 +0100

    freebsd support added

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
-
+USERNAME=`whoami`
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+HOME=`/bin/sh -fc "echo ~$USERNAME"`
 
-ln -s $DIR/zsh/zshrc ~/.zshrc
+ln -s $DIR/zsh/zshrc $HOME/.zshrc
 
-ln -s $DIR/vim ~/.vim
-ln -s $DIR/vim/vimrc ~/.vimrc
-ln -s $DIR/vim/gvvimrc ~/.gvimrc
+ln -s $DIR/vim $HOME/.vim
+ln -s $DIR/vim/vimrc $HOME/.vimrc
+ln -s $DIR/vim/gvvimrc $HOME/.gvimrc
 
-ln -s $DIR/tmux/tmux.conf ~/.tmux.conf
+ln -s $DIR/tmux/tmux.conf $HOME/.tmux.conf
 
-ln -s $DIR/git/gitconfig ~/.gitconfig
-ln -s $DIR/git/gitconfig-local ~/.gitconfig-local
+ln -s $DIR/git/gitconfig $HOME/.gitconfig
+ln -s $DIR/git/gitconfig-local $HOME/.gitconfig-local
 
-touch ~/.z
+touch $HOME/.z
 
 cd $DIR && git submodule update --init --recursive
 
 echo "Use ~/.gitconfig-local for git settings not to be tracked by git, e.g. 'name' and 'email'."
+
